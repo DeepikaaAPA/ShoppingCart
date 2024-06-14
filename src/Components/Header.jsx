0e3799ab129f1b69f3ffa8804a49c085e3ecc6cb
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-14 17:13:00 +0530

    fix:header name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,10 @@ export function Header() {
       <header className="bg-dark py-1">
         <div className="container px-4 px-lg-5 my-3">
           <div className="text-center text-white">
-            <h1 className="display-4 fw-bolder">Shop in style</h1>
-            <p className="lead fw-normal text-white-50 mb-0">From ...AAA to ZZZ...</p>
+            <h1 className="display-4 fw-bolder">Shop In Style</h1>
+            <p className="lead fw-normal text-white-50 mb-0">
+              From ...A to Z...
+            </p>
           </div>
         </div>
       </header>
