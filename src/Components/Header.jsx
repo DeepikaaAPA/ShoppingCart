60545754691fbd2152a78b77622806bbbf41396d
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-14 16:19:51 +0530

    update: reducing padding and margin

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <>
-      <header className="bg-dark py-5">
-        <div className="container px-4 px-lg-5 my-5">
+      <header className="bg-dark py-1">
+        <div className="container px-4 px-lg-5 my-3">
           <div className="text-center text-white">
             <h1 className="display-4 fw-bolder">Shop in style</h1>
             <p className="lead fw-normal text-white-50 mb-0">From ...AAA to ZZZ...</p>
